fb27136ad5b7a50b8a59eecdd3242a981d74a023
ckw10326 <81143251+ckw10326@users.noreply.github.com>
2024-06-30 23:37:18 +0800

    a

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import twstock
-stock = twstock.Stock('2330')
-print(stock.sid)
-print(stock.price)
-print(stock.high)
-print(stock.date)
+twselist = []#上市代號
+tpexliet = []#上櫃代號
+
+for i in twstock.twse:
+    if len(i) == 4:
+        twselist.append(i)
+
+for i in twstock.tpex:
+    if len(i) == 4:
+        tpexliet.append(i)
+
+print("上市股票數量:",len(twselist))
+for i in twselist:
+    print(i)
+print("上櫃股票數量:",len(tpexliet))
+for i in tpexliet:
+    print(i)
